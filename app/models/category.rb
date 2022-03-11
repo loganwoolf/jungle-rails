b5c8bfb7fb0abd35b0ab8aa47c7392b173acae9e
Logan Woolf <loganwoolf@gmail.com>
2022-03-10 22:43:29 -0700

    Add validation to form inputs

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,7 @@ class Category < ActiveRecord::Base
 
   has_many :products
 
+  validates :name, length: { in: 4..32 }
+  validates :name, format: { with: /\A[a-zA-Z][a-z A-Z]+[a-zA-Z]\z/,
+    message: "can only be letters with spaces between" }
 end
