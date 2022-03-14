f4d9400afbb10a2ed24cc42c5f37351a6570200a
Logan Woolf <loganwoolf@gmail.com>
2022-03-14 14:45:31 -0600

    Add logic to register user in Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params) # see private method
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/users/new'
+    end
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
