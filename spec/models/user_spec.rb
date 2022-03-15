71450a899efdedbd891b32092c005410d54414df
Logan Woolf <loganwoolf@gmail.com>
2022-03-15 14:01:41 -0600

    Add Authentication tests, extra password confirm'n
    Use .new instead of .create where possible

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "Validations" do
     it "Should save successfully with everything set" do
-      new_user = User.create({
+      new_user = User.new({
         :email => 'soap_dodger@gmail.com',
         :first_name => 'Soap',
         :last_name => 'Dodger',
@@ -15,7 +15,7 @@ RSpec.describe User, type: :model do
     end
 
     it "Should not save if first name is missing" do
-      new_user = User.create({
+      new_user = User.new({
         :email => 'soap_dodger@gmail.com',
         # :first_name => 'Soap',
         :last_name => 'Dodger',
@@ -23,11 +23,11 @@ RSpec.describe User, type: :model do
         :password_confirmation => 'paranormal'
       })
 
-      expect(new_user).to be_invalid
+      expect(new_user).not_to be_valid
     end
 
     it "Should not save if last name is missing" do
-      new_user = User.create({
+      new_user = User.new({
         :email => 'soap_dodger@gmail.com',
         :first_name => 'Soap',
         # :last_name => 'Dodger',
@@ -35,11 +35,11 @@ RSpec.describe User, type: :model do
         :password_confirmation => 'paranormal'
       })
 
-      expect(new_user).to be_invalid
+      expect(new_user).not_to be_valid
     end
 
     it "Should not save if email is missing" do
-      new_user = User.create({
+      new_user = User.new({
         # :email => 'soap_dodger@gmail.com',
         :first_name => 'Soap',
         :last_name => 'Dodger',
@@ -47,7 +47,7 @@ RSpec.describe User, type: :model do
         :password_confirmation => 'paranormal'
       })
 
-      expect(new_user).to be_invalid
+      expect(new_user).not_to be_valid
     end
 
     it "Should not save if email already exists" do
@@ -59,7 +59,7 @@ RSpec.describe User, type: :model do
         :password_confirmation => 'paranormal'
       })
 
-      new_user = User.create({
+      new_user = User.new({
         :email => 'soap_dodger@gmail.com',
         :first_name => 'Soap',
         :last_name => 'Dodger',
@@ -67,11 +67,11 @@ RSpec.describe User, type: :model do
         :password_confirmation => 'paranormal'
       })
 
-      expect(new_user).to be_invalid
+      expect(new_user).not_to be_valid
     end
 
     it "Should not save if passwords do not match" do
-      new_user = User.create({
+      new_user = User.new({
         :email => 'soap_dodger@gmail.com',
         :first_name => 'Soap',
         :last_name => 'Dodger',
@@ -79,8 +79,48 @@ RSpec.describe User, type: :model do
         :password_confirmation => 'hypernormal'
       })
 
-      expect(new_user).to be_invalid
+      expect(new_user).not_to be_valid
     end
 
+    it "Should not save if password confirmation missing" do
+      new_user = User.new({
+        :email => 'soap_dodger@gmail.com',
+        :first_name => 'Soap',
+        :last_name => 'Dodger',
+        :password => 'paranormal',
+        :password_confirmation => ''
+      })
+      expect(new_user).not_to be_valid
+    end
   end
+
+  describe "Authentication" do
+    before do
+      new_user = User.create({
+        :email => 'soap_dodger@gmail.com',
+        :first_name => 'Soap',
+        :last_name => 'Dodger',
+        :password => 'rightpassword',
+        :password_confirmation => 'rightpassword'
+      })
+    end
+
+    it "should return an instance of the authenticated user" do
+      compare_user = User.find_by_email('soap_dodger@gmail.com')
+
+      auth_user = User.authenticate_with_credentials('soap_dodger@gmail.com', 'rightpassword')
+
+      # expect(auth_user).to be_an_instance_of User
+      expect(auth_user).to eq(compare_user)
+    end
+
+    it "should return nil if user not authenticated" do
+      auth_user = User.authenticate_with_credentials('soap_dodger@gmail.com', 'wrongpassword')
+
+      expect(auth_user).to be_nil
+    end
+  end
+
 end
+
+# expect(user.password_digest).to eq(BCrypt::Password.new(password))
