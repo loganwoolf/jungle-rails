03e688176529597391d13e36ac83ada486c73ae7
Logan Woolf <loganwoolf@gmail.com>
2022-03-15 20:45:05 -0600

    Test for navigating to product details page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "Can navigate to product details page" do
+    # ACT
+    visit root_path
+    first(".product a").click
+    # DEBUG / VERIFY
+    expect(page).to have_css 'article.product-detail', count: 1
+    save_screenshot
+  end
+
+
+end
