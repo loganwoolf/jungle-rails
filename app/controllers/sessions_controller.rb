2467e4c1c325de6c43335b35a41e6bd134dd762a
Logan Woolf <loganwoolf@gmail.com>
2022-03-16 16:53:50 -0600

    Show errors in template if login is unsuccessful

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,12 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to '/login'
+      redirect_to login_url(message: 'Email and/or password not valid.')
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login'
+    redirect_to login_url
   end
 end
